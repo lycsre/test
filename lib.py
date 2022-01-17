38bc194d36ac64a3b393b6a5627341465a06c084
lycsre <91449337+lycsre@users.noreply.github.com>
2022-01-17 21:43:03 +0800

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,11 @@
+def add(a:str, b:str) -> str: 
+    return str(a + b + 1)
+
+def sub(a, b) -> int:
+    return a - b
+
+def mul(a, b) -> int:
+    return a * b
+
+def div(a, b) -> int:
+    return a // b
